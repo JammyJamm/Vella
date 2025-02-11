4bbd2ade9affb090f64c99bac5e8eba24195d61f
SenthamilMunusamy <senthamil.munusamy@global-csg.com>
2025-02-11 11:52:09 +0530

    #updated Add file

--- src/pages/AddData.js
+++ src/pages/AddData.js
@@ -1,22 +1,25 @@
-import { addDoc, collection } from "firebase/firestore";
-import { Link } from "react-router-dom";
+import React from "react";
+import { collection, addDoc } from "firebase/firestore";
 import db from "./FirbaseConfig";
-
-const AddData = () => {
+import back from "../assets/images/back.svg";
+import background from "../assets/images/Background.png";
+import { Link } from "react-router-dom";
+const addData = () => {
+  // const colRef = collection(db, "player");
   const handleLadduAddMatch = (e) => {
     e.preventDefault();
     const addForm = document.querySelector("#addLadduData");
 
     const colRef = collection(db, "Laddu");
     addDoc(colRef, {
-      league: addForm.league.value,
-      player1Score: addForm.player1Score.value,
-      player2Score: addForm.player2Score.value,
       player1Name: addForm.player1Name.value,
-      player2Name: addForm.player2Name.value,
       player1Role: addForm.player1Role.value,
+      player1Score: addForm.player1Score.value,
+      player2Name: addForm.player2Name.value,
       player2Role: addForm.player2Role.value,
-      totalScore: addForm.totalScore.value,
+      player2Score: addForm.player2Score.value,
+      league: addForm.league.value,
+      result: addForm.result.value,
     })
       .then((response) => {
         alert("Added");
@@ -29,50 +32,126 @@ const AddData = () => {
     return e.target.value;
   };
   return (
-    <div className="Add-layout">
-      {/* Nav Bar Starts */}
-
-      <nav className="top-nav">
-        <div className="title">
+    <div className="ui-addLadduMatch" id="addWapper">
+      <div className="cover" style={{ backgroundImage: `url(${background})` }}>
+        <h2 className="title">Add Laddu Match</h2>
+        <div className="close">
           <Link to={"/"}>
-            <img
-              src="https://github.com/JammyJamm/JSON_Data/blob/main/logo_senthamil.png?raw=true"
-              alt="logo"
-            />
+            <img src={back} alt="back" />
           </Link>
-          <div className="add">
-            <Link to={"/add"}>
-              <button className="add-btn">
-                <img
-                  src="https://raw.githubusercontent.com/JammyJamm/JSON_Data/363a4623ccb014c36ace59c04e6cb6979ad0d708/add.svg"
-                  alt="add"
-                />
-              </button>
-            </Link>
+        </div>
+      </div>
+      <div className="content">
+        <div className="card">
+          <div className="ui-add-data">
+            <div className="form-elements">
+              <form
+                id="addLadduData"
+                onSubmit={handleLadduAddMatch}
+                className="row"
+              >
+                <div className="col-6 col-sm-6 col-md-4">
+                  <label className="form-label">Player1 Name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="player1Name"
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div className="col-6 col-sm-6 col-md-4">
+                  <label className="form-label">Player1 Role</label>
+                  <select
+                    type="text"
+                    className="form-control"
+                    name="player1Role"
+                    onChange={handleChange}
+                    required
+                  >
+                    <option value="wk">Wicket Keeper</option>
+                    <option value="captain">Captain</option>
+                    <option value="player">Player</option>
+                  </select>
+                </div>
+                <div className="col-6 col-sm-6 col-md-4">
+                  <label className="form-label">Player1 Score</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="player1Score"
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div className="col-6 col-sm-6 col-md-4">
+                  <label className="form-label">Player2 Name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="player2Name"
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div className="col-6 col-sm-6 col-md-4">
+                  <label className="form-label">Player2 Role</label>
+                  <select
+                    type="text"
+                    className="form-control"
+                    name="player2Role"
+                    onChange={handleChange}
+                    required
+                  >
+                    <option value="wk">Wicket Keeper</option>
+                    <option value="captain">Captain</option>
+                    <option value="player">Player</option>
+                  </select>
+                </div>
+                <div className="col-6 col-sm-6 col-md-4">
+                  <label className="form-label">Player2 Score</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="player2Score"
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div className="col-6 col-sm-6 col-md-4">
+                  <label className="form-label">League</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="league"
+                    required
+                    value={"BPL_W"}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="col-6 col-sm-6 col-md-4">
+                  <label className="form-label">Result</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="result"
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+
+                <div
+                  className="d-grid btn-group justify-content-center col-12 mx-auto"
+                  style={{ margin: "16px" }}
+                >
+                  <button className="btn btn-primary blue-btn">Add Data</button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
-        <div className="main">
-          <div className="main-content">
-            <form
-              id="addLadduData"
-              onSubmit={handleLadduAddMatch}
-              className="row"
-            >
-              <div className="col-6 col-sm-6 col-md-4">
-                <label className="form-label">Player1 Name</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="player1Name"
-                  onChange={handleChange}
-                  required
-                />
-              </div>
-            </form>
-          </div>
-        </div>
-      </nav>
+      </div>
     </div>
   );
 };
-export default AddData;
+export default addData;
